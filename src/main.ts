11ce36fe721dd82d64120eaafcd016d6cf34c97e
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2020-10-17 15:09:12 -0700

    sketching out the concept

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import * as core from '@actions/core'
+import * as core from '@actions/core';
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+    const bookDirectory: string = core.getInput('directory');
+    const releaseName: string = core.getInput('releasename');
+    const bookName: string = core.getInput('bookname');
+    const languageId: string = core.getInput('languageid');
+
+    // zip
+    
+
+    // tar
+
+    // get datestring
+
+    // create release
+
+    // upload zip
+
+    // upload tar
+
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
